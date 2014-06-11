290d80bdde64d71e0fb4a0df45acb9601b97ab9e
Phillipe Cavalcante <cavalcante.phillipe@gmail.com>
2014-06-11 13:06:38 -0300

    Pequenas mudanças em subscribe.html, views.py e base.html

--- apps/survey/views.py
+++ apps/survey/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import View
+
+from apps.survey.forms import SubscribeForm
+
 # Create your views here.
 
 class SubscribeView(View):
 
     def get(self, request):
-        return render(request, 'survey/subscribe.html')
+        data = {'form' : SubscribeForm()}
+        return render(request, 'survey/subscribe.html', data)
+
+    def post(self, request):
+
+        form = SubscribeForm(request.POST)
+        data = {'form' : SubscribeForm()}
+        
+        if form.is_valid():
+            return render(request, 'survey/subscribe.html', data)
+        else:
+            data.update({'x':'fasdfs'})
+            return render(request, 'survey/subscribe.html', data)
