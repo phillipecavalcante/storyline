94a4fa46d1890bf72b373df8a25d5bc69159db15
Phillipe Cavalcante <cavalcante.phillipe@gmail.com>
2014-06-17 15:29:31 -0300

    app search adicionado  para dar continuidade ao app survey que precisa do modelo Article

---
+++ apps/search/models.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+
+    source = models.CharField(max_length=255)
+    url = models.URLField()
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(auto_now_add=False)
+    description = models.TextField()
+    body = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
+class Topic(models.Model):
+
+    name = models.CharField(max_length=255)
+    terms = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+        
