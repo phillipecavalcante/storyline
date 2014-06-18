d887f8433b5ac86b946d1195434fa53ba410cbaf
Phillipe Cavalcante <cavalcante.phillipe@gmail.com>
2014-06-18 12:37:54 -0300

    App search adicionado e url de busca configurada.

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     # third-party
     'captcha',
     # apps
+    'apps.search',
     'apps.survey',
 )
 
--- project/urls.py
+++ project/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('apps.search.urls', namespace='search')),
     url(r'^survey/', include('apps.survey.urls', namespace='survey')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
